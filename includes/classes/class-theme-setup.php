9dcd68285c4dafca3df69b5e33fed0990a6a57fd
Mikael Eriksson <mikael.eriksson.nkpg@gmail.com>
2024-11-07 15:14:54 +0100

    Removing core patterns.

--- includes/classes/class-theme-setup.php
+++ includes/classes/class-theme-setup.php
@@ -16,6 +16,7 @@ class Theme_Setup extends Loader {
     public function init() : void {
         add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_styles' ] );
         add_action( 'init', [ $this, 'register_theme_blocks' ] );
+        add_action( 'init', [ $this, 'remove_core_patterns' ] );
     }
 
     /**
@@ -40,5 +41,15 @@ class Theme_Setup extends Loader {
     public function register_theme_blocks() : void {
         register_block_type( get_stylesheet_directory() . '/build/blocks/arrow-link/block.json' );
         register_block_type( get_stylesheet_directory() . '/build/blocks/navigation/block.json' );
+        register_block_type( get_stylesheet_directory() . '/build/blocks/project-header/block.json' );
+    }
+
+    /**
+     * Removes core patterns.
+     * 
+     * @return void
+     */
+    public function remove_core_patterns() : void {
+        remove_theme_support( 'core-block-patterns' );
     }
 }
