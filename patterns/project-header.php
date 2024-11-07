a7960ad1482eb8b407259903b817a362bd6b790b
Mikael Eriksson <mikael.eriksson.nkpg@gmail.com>
2024-11-07 20:18:11 +0100

    Adding project header pattern

---
+++ patterns/project-header.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Project Header
+ * Slug: btp/project-header
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="font-style:normal;font-weight:700;text-transform:uppercase">Project</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading">Add a stunning heading for the project page here</h1>
+<!-- /wp:heading -->
+
+<!-- wp:post-date {"style":{"elements":{"link":{"color":{"text":"var:preset|color|tertiary"}}}},"textColor":"tertiary"} /-->
+
+<!-- wp:post-excerpt {"style":{"typography":{"fontSize":"24px"}}} /-->
+
+<!-- wp:post-featured-image /-->
+
+</div>
+<!-- /wp:group -->
